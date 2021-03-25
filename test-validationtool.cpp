02eecd86271e1b480c8022008fd1040c5a7fba4f
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-03-25 22:22:01 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -17,5 +17,5 @@ TEST_CASE("reports error when current jumps abruptly") {
 
 TEST_CASE("reports error when invalid data is passed") {
   double * currentReadings = NULL;
-  REQUIRE(validateCurrentreadings(currentReadings, 0) == false);
+  REQUIRE(validateCurrentreadings(currentReadings, 0) == true);
 }
